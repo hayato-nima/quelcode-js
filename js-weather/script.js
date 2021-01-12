2e758437d2e2be1daa4f2db3f2a57b2bc92a21eb
hayato-nima <kdc98bac72xkx@gmail.com>
2021-01-12 10:16:24 +0900

    add:ブラウザでの天気、気温、湿度の表示処理を作成

--- js-weather/script.js
+++ js-weather/script.js
@@ -1,42 +1,59 @@
 'use strict';
-//現在の天気を取得する場所の名前
 
+//select要素の取得
+let select = document.querySelector("#select");
 
+//option要素の取得（配列）
+let options = document.querySelectorAll("#select option");
 
-let targetCityName = "london";
-let appId = "4b5774e9f3d2a07b84f0f2f88e486224";
+//select要素のchangeイベントの登録
+select.addEventListener('change', function () {
 
-const requestUrl = "https://api.openweathermap.org/data/2.5/weather?APPID=" + appId + "&lang=ja&units=metric&q=" + targetCityName + ",jp;";
+  //選択されたoption番号を取得
+  let index = this.selectedIndex;
 
-//Ajax通信用のオブジェクトを作成
-let xhr = new XMLHttpRequest();
+  //現在の天気を取得する場所の名前
+  let targetCityName = options[index].value;
+  let appId = "4b5774e9f3d2a07b84f0f2f88e486224";
 
-//通信方式とURLを設定
-xhr.open("GET", requestUrl);
+  const requestUrl = "https://api.openweathermap.org/data/2.5/weather?APPID=" + appId + "&lang=ja&units=metric&q=" + targetCityName + ",jp;";
 
-//通信を実行する
-xhr.send();
+  //Ajax通信用のオブジェクトを作成
+  let xhr = new XMLHttpRequest();
 
-//通信ステータスが変わったら実行される関数
-xhr.onreadystatechange = function () {
-  //通信が完了
-  if (xhr.readyState == 4) {
-    ShowTodaysWeather(xhr.responseText);
+  //通信方式とURLを設定
+  xhr.open("GET", requestUrl);
+
+  //通信を実行する
+  xhr.send();
+
+  //通信ステータスが変わったら実行される関数
+  xhr.onreadystatechange = function () {
+    //通信が完了
+    if (xhr.readyState == 4) {
+      ShowTodaysWeather(xhr.responseText);
+    }
   }
-}
 
-/**
- * 今日の天気を表示する
- */
-function ShowTodaysWeather(response) {
+  // 今日の天気を表示する
+  function ShowTodaysWeather(response) {
 
-  let obj = JSON.parse(response);
+    let obj = JSON.parse(response);
 
-  let weather = obj.weather[0].description;
-  let city = obj.name;
-  let temp = obj.main.temp;
+    let weather = obj.weather[0].description;
+    let city = obj.name;
+    let temp = obj.main.temp;
+    let humidity = obj.main.humidity;
 
-  console.log("現在の" + city + "の天気は" + weather);
-  console.log("気温は" + temp + "度です。");
+    console.log(obj);
+    document.getElementById('cityweather').textContent = `
+    現在の${city}の天気：${weather}`
+    document.getElementById('temp').textContent = `
+    気温：${temp}°C`
+    document.getElementById('humidity').textContent = `
+    湿度：${humidity}％`
 
-}
+
+  }
+
+});
